2595d8921c5d6b2092527f0b18dd95f09481f01d
1Zaytsev <4yu@bk.ru>
2020-07-07 01:25:36 +0300

    RabbitMQ.

--- src/main/java/qdo_ln/q_shop/QShopApplication.java
+++ src/main/java/qdo_ln/q_shop/QShopApplication.java
@@ -1,10 +1,45 @@
 package qdo_ln.q_shop;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import qdo_ln.q_shop.beans.Receiver;
 
 @SpringBootApplication
 public class QShopApplication {
+    public static final String EXCHANGE_NAME = "exchanger";
+
+    @Bean
+    Queue queue(){
+        return new Queue("Order_checkout", true, false, true);
+    }
+
+    @Bean
+    TopicExchange topicExchange(){
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    Binding bingQueue(Queue queue, TopicExchange topicExchange){
+        return BindingBuilder.bind(queue).to(topicExchange).with("checkout");
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver){
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer containerForTopic(org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames("Order_checkout");
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(QShopApplication.class, args);
